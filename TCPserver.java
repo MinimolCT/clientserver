e3483d8038f244649e355463566d31538050c741
Mini C T <36188453+MinimolCT@users.noreply.github.com>
2018-03-23 05:34:21 +0000

    Update TCPserver.java

--- TCPserver.java
+++ TCPserver.java
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-=======
 import java.io.*;
 import java.net.*;
 
-<<<<<<< HEAD:server.java
->>>>>>> 3c353672f65bac4746428db5d61020c3c1da7fc4
-class TCPServer {
-=======
 class TCPserver {
->>>>>>> 074593f6fcc42ca8cadd347873a266d009fba6b0:TCPserver.java
  public static void main(String argv[]) throws Exception {
   String clientSentence;
   String capitalizedSentence;
   ServerSocket welcomeSocket = new ServerSocket(6789);
-<<<<<<< HEAD
-while (true) {
-   Socket connectionSocket = welcomeSocket.accept();
-=======
 
   while (true) {
    Socket connectionSocket = welcomeSocket.accept();
@@ -28,6 +17,5 @@ while (true) {
    capitalizedSentence = clientSentence.toUpperCase() + '\n';
    outToClient.writeBytes(capitalizedSentence);
   }
->>>>>>> 3c353672f65bac4746428db5d61020c3c1da7fc4
 }
 }
